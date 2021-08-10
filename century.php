f501f4105feb58b7ed549a27c417c61a641e86f6
Rodrigo-Maia <62244397+Rodrigo-Maia@users.noreply.github.com>
2021-08-10 14:53:32 -0300

    Update century.php

--- century.php
+++ century.php
@@ -1,3 +1,11 @@
 <?php
 $centure = '';
-$ano = filter_input
+$ano = filter_input(INPUT_POST,'ano',FILTER_SANITIZE_NUMBER_INT) ;
+        $arredondamento = $ano % 100;
+//echo $arredondamento;
+        $century = round($ano/100);
+        if ($arredondamento>50) {
+            echo $century;
+        } else {
+            echo $century + 1;
+        }
